ffbfa158fdb447e1abf3d45f7af8d2dca757428e
Danheon <eksgjs3847@gmail.com>
2022-11-28 09:54:11 +0900

    x-y-axis

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useRef, useEffect, useState, Fragment } from "react";
 import "./App.css";
-import { select, line, curveCardinal, axisBottom, scaleLinear } from "d3";
+import {
+  select,
+  line,
+  curveCardinal,
+  axisBottom,
+  axisRight,
+  scaleLinear,
+} from "d3";
 
 function App() {
-  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
   const svgRef = useRef();
 
   useEffect(() => {
@@ -11,42 +18,42 @@ function App() {
     const xScale = scaleLinear()
       .domain([0, data.length - 1])
       .range([0, 300]);
-    const yScale = scaleLinear().domain([0, 75]).range([150, 0]);
+    const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
 
-    const xAxis = axisBottom(xScale);
+    // x축
+    const xAxis = axisBottom(xScale)
+      .ticks(data.length)
+      .tickFormat((index) => index + 1);
+    svg.select(".x-axis").style("transform", "translateY(150px)").call(xAxis);
+    // call
+    // xAxis(svg.select(".x-axis"))
 
+    // y축
+    const yAxis = axisRight(yScale);
+    svg.select(".y-axis").style("transform", "translateX(300px)").call(yAxis);
+
+    // generates the "d" attribute of a path element
     const myLine = line()
       .x((_, index) => xScale(index))
       .y(yScale)
       .curve(curveCardinal);
+
+    // renders path element, and attaches
+    // the "d" attribute from line generator above
     svg
-      .selectAll("path")
+      .selectAll(".line")
       .data([data])
       .join("path")
+      .attr("class", "line")
       .attr("d", (value) => myLine(value))
       .attr("fill", "none")
       .attr("stroke", "blue");
-    // svg
-    // .selectAll("circle")
-    // .data(data)
-    // .join("circle")
-    // .join(
-    //   (enter) => enter.append("circle").attr("class", "new"),
-
-    //   (update) => update.attr("class", "updated"),
-    //   (exit) => exit.remove()
-    // )
-    // enter, update 시에 둘다적용됨
-    // .attr("r", (value) => value)
-    // .attr("cx", (value) => value * 2)
-    // .attr("cy", (value) => value * 2)
-    // .attr("stroke", "red")
-    // .attr("fill", "none");
   }, [data]);
   return (
     <Fragment>
       <svg ref={svgRef}>
-        <path d="M0,150 100,100 150,120" stroke="blue" fill="none" />
+        <g className="x-axis" />
+        <g className="y-axis" />
       </svg>
       <br />
       <button onClick={() => setData(data.map((value) => value + 5))}>
